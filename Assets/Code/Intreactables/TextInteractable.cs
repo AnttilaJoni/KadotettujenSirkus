6423593cc8e18f24f79768bef7dd3488072116fd
AnttilaJoni <jonianttila110@gmail.com>
2026-05-11 12:40:21 +0300

    Interact alkuun

---
+++ Assets/Code/Intreactables/TextInteractable.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextInteractable : MonoBehaviour, IInteractable
+{
+    public bool IsInteracted { get; private set;}
+    public string InteractID { get; private set;}
+    
+    public Sprite interactedSprite;
+
+    void Start()
+    {
+        InteractID ??= GlobalHelper.GenerateUniqueID(gameObject);
+    }
+
+
+    public bool CanInteract()
+    {
+        return !IsInteracted;
+    }
+    public void Interact()
+    {
+       if(!CanInteract()) return;
+       OpenInteract();
+    }
+
+
+    private void OpenInteract()
+    {
+        SetOpened(true);
+        
+    }
+    public void SetOpened(bool opened)
+    {
+        if(IsInteracted = opened)
+        {
+            GetComponent<SpriteRenderer>().sprite = interactedSprite;
+        }
+    }
+}
