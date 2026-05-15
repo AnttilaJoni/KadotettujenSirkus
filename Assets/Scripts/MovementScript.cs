bfbe114207695cae23c03808fb6fa9d22f01fdf5
AnttilaJoni <jonianttila110@gmail.com>
2026-05-15 12:57:01 +0300

    Animaatiot toimii

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -1,4 +1,5 @@
-using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MovementScript : MonoBehaviour
@@ -7,6 +8,12 @@ public class MovementScript : MonoBehaviour
 
     public float moveSpeed = 5f;
     public Vector3 moveDirection;
+
+    private Vector2 input;
+
+    [SerializeField ]Animator anim;
+    private Vector2 lastMoveDirection;
+    
     
     void Start()
     {
@@ -17,14 +24,36 @@ public class MovementScript : MonoBehaviour
     void Update()
     {
         Inputs();
+        Animate();
     }
 
     void Inputs()
     {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        if((moveX == 0 && moveY == 0) && (input.x != 0 || input.y != 0))
+        {
+            lastMoveDirection = input;
+        }
+
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+        input.Normalize();
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         
         moveDirection = new Vector3(horizontal, vertical,  0);
+    }
+    void Animate()
+    {
+        anim.SetFloat("MoveX", input.x);
+        anim.SetFloat("MoveY", input.y);
+        anim.SetFloat("MoveMagnitude", input.magnitude);
+        anim.SetFloat("LastMoveX", lastMoveDirection.x);
+        anim.SetFloat("LastMoveY", lastMoveDirection.y);
     }
 
     private void FixedUpdate()
