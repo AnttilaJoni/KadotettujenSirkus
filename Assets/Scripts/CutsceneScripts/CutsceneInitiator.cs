469cfcc21430e046c7f4c6c26a7f88fc0960815b
AnttilaJoni <jonianttila110@gmail.com>
2026-06-04 13:44:19 +0300

    Save asiat play buttonista cutscene scriptiin

--- Assets/Scripts/CutsceneScripts/CutsceneInitiator.cs
+++ Assets/Scripts/CutsceneScripts/CutsceneInitiator.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,26 @@ public class CutsceneInitiator : MonoBehaviour
 {
     private CutsceneHandler cutsceneHandler;
     private bool playPressed;
+    private string _saveLocation;
 
     public void Start()
     {
+        _saveLocation = Path.Combine(Application.persistentDataPath, "saveData.json");
+        try {
+            if (File.Exists(_saveLocation)) {
+                File.Delete(_saveLocation);
+                Debug.Log("Save file deleted");
+            }
+
+            else {
+                Debug.Log("No save file found");
+            }
+        }
+
+        catch (DirectoryNotFoundException) {
+            Debug.Log("File not found");
+        }
+
         cutsceneHandler = GetComponent<CutsceneHandler>();
         playPressed = false;
     }
