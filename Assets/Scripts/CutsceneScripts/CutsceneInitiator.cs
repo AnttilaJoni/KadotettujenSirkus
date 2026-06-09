e8aae5cd4c568f70646002c8bc19eb5248737830
AnttilaJoni <jonianttila110@gmail.com>
2026-06-09 10:59:31 +0300

    Ending setup valmiina

--- Assets/Scripts/CutsceneScripts/CutsceneInitiator.cs
+++ Assets/Scripts/CutsceneScripts/CutsceneInitiator.cs
@@ -2,15 +2,25 @@ using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CutsceneInitiator : MonoBehaviour
 {
     private CutsceneHandler cutsceneHandler;
     private bool playPressed;
+    private bool endingStarted;
     private string _saveLocation;
 
     public void Start()
     {
+        cutsceneHandler = GetComponent<CutsceneHandler>();
+        Scene scene = SceneManager.GetActiveScene();
+        if(scene.name == $"Endings" && !endingStarted)
+        {   
+            StartCoroutine(OnEndingStart());
+            endingStarted = true;
+        }
+
         _saveLocation = Path.Combine(Application.persistentDataPath, "saveData.json");
         try {
             if (File.Exists(_saveLocation)) {
@@ -26,8 +36,6 @@ public class CutsceneInitiator : MonoBehaviour
         catch (DirectoryNotFoundException) {
             Debug.Log("File not found");
         }
-
-        cutsceneHandler = GetComponent<CutsceneHandler>();
         playPressed = false;
     }
 
@@ -36,7 +44,11 @@ public class CutsceneInitiator : MonoBehaviour
         if(other.gameObject.tag == "Player")
             cutsceneHandler.PlayNextElement();
     }
-
+    private IEnumerator OnEndingStart()
+    {
+        yield return new WaitForSeconds(2f);
+        cutsceneHandler.PlayNextElement();
+    }
     public void OnStartGameClicked()
     {
         if(playPressed)
