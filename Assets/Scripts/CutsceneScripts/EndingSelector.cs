ecc0c06b4a6969c0ac40c81b2f3f44e3b23a8cc2
AnttilaJoni <jonianttila110@gmail.com>
2026-06-09 14:09:10 +0300

    Kun läpäisee pelin -> poistaa savefilen ja alkaa uudesta

--- Assets/Scripts/CutsceneScripts/EndingSelector.cs
+++ Assets/Scripts/CutsceneScripts/EndingSelector.cs
@@ -1,21 +1,45 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 
 public class EndingSelector : MonoBehaviour
 {
     public GameObject neutral;
     public GameObject good;
+    private string _saveLocation;
     
     void Start()
     {
+        _saveLocation = Path.Combine(Application.persistentDataPath, "saveData.json");
         if(GameObject.FindGameObjectWithTag("PlayerStats").GetComponent<DontDestroyOnLoad>().gameEnding == 2) //<-- Good ending saveState
         {
             good.SetActive(true);
+            DeleteSaveData();
+
         }
         else
         {
             neutral.SetActive(true);
+            DeleteSaveData();
+        }
+    }
+
+    void DeleteSaveData()
+    {
+        try {
+            if (File.Exists(_saveLocation)) {
+                File.Delete(_saveLocation);
+                Debug.Log("Save file deleted");
+            }
+
+            else {
+                Debug.Log("No save file found");
+            }
+        }
+
+        catch (DirectoryNotFoundException) {
+            Debug.Log("File not found");
         }
     }
 }
